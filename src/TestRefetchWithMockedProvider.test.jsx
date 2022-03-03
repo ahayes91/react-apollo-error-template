33b8309d1a9c3f86b16cb581e23fdb425bf8425c
Aislinn Hayes <aislinn.hayes@workday.com>
2022-03-03 15:09:02 +0000

    Update the test so that it shows the mocked refetch correctly

--- src/TestRefetchWithMockedProvider.test.jsx
+++ src/TestRefetchWithMockedProvider.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MockedProvider } from '@apollo/client/testing';
 import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { App, ALL_PEOPLE, GET_PERSON, ADD_PERSON } from './App';
 import '@testing-library/jest-dom/extend-expect';
 import { peopleData } from './client';
@@ -22,11 +23,28 @@ const mockGetPersonQuery = {
             name: '',
         }
     },
-    result: jest.fn(() => ({
+    newData: jest.fn(() => ({
         data: {
             person: {
                 id: 7,
                 name: 'Amy Poehler',
+            }
+    }
+    })),
+};
+
+const mockGetPersonQueryAfterSearch = {
+    request: {
+        query: GET_PERSON,
+        variables: {
+            name: 'John Smith',
+        }
+    },
+    newData: jest.fn(() => ({
+        data: {
+            person: {
+                id: 1,
+                name: 'John Smith',
             }
     }
     })),
@@ -39,11 +57,22 @@ describe('Apollo Mocked Provider when refetch is called with different variables
                 mocks={[
                     mockAllPeopleQuery,
                     mockGetPersonQuery,
+                    mockGetPersonQueryAfterSearch,
                 ]}>
                 <App />
             </MockedProvider>
         );
         await waitFor(() => expect(screen.queryByText('Loading…')).not.toBeInTheDocument());
         expect(screen.getByRole('heading', { level: 1, name: 'Apollo Client Issue Reproduction'})).toBeInTheDocument();
+        expect(mockGetPersonQuery.newData).toHaveBeenCalledTimes(1);
+        const inputBoxes = screen.getAllByRole('textbox');
+        expect(inputBoxes).toHaveLength(2);
+        const searchNameInput = inputBoxes[1];
+        userEvent.type(searchNameInput, 'John Smith');
+        const button = screen.getByRole('button', { name: 'Search person'});
+        expect(button).toBeInTheDocument();
+        userEvent.click(button);
+        await waitFor(()=> expect(mockGetPersonQuery.newData).toHaveBeenCalledTimes(1));
+        await waitFor(()=> expect(mockGetPersonQueryAfterSearch.newData).toHaveBeenCalledTimes(1));
     });
 });
